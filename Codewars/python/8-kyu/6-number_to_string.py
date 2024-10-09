b27d8f1555f577b297627a8e4f435fbe61cde152
aimanyusufw <aimanyusufdev@gmail.com>
2024-10-09 07:48:50 +0700

    finish : <python> codewars 6 level 8kyu Covert a number to a string

---
+++ Codewars/python/8-kyu/6-number_to_string.py
@@ -0,0 +1,24 @@
+# Codewars 6
+# Title : Convert a number to a string
+# Question : We need a function to conver a number(integer) into a string
+# Challange Url : https://www.codewars.com/kata/5265326f5fda8eb1160004c8/
+# Level : 8kyu
+
+# Method : 1
+def number_to_string(number):
+    return str(number)
+
+# Method : 2
+def number_to_string(number): return str(number)
+
+# Other Users Method
+def number_to_string(num):
+    return "{}".format(num)
+
+# Log or debug
+print(number_to_string(202))
+print(number_to_string(79585))
+print(number_to_string(79585))
+print(number_to_string(1+2))
+print(number_to_string(1-2))
+

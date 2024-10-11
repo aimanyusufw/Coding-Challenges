57ded73966b1ff5bf9f84154f2bd0626ed7827b1
aimanyusufw <aimanyusufdev@gmail.com>
2024-10-11 12:40:06 +0700

    finish : <java> codewars 4 level 8kyu Function 1 - Hello world

---
+++ Leetcode/Javascript/5-longestPalindrome.js
@@ -0,0 +1,15 @@
+// Leetcode 5
+// Title : Logest Palindrom Substring
+// Instructions : Given a string s, return the longest palindromic substring in s.
+// Challange Url : https://leetcode.com/problems/longest-palindromic-substring/
+// Level : Medium
+
+// Method : 1
+function longestPalindrome(s) {}
+
+// Other Users Method
+
+// Result or debug
+console.log(longestPalindrome("ababs"));
+console.log(longestPalindrome("babad"));
+console.log(longestPalindrome("cbbd"));

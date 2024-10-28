a270ebb4be43fcf27df2969892912addf82f2675
aimanyusufw <aimanyusufdev@gmail.com>
2024-10-28 08:24:53 +0700

    ref : refacto nested describe

--- Codewars/typescript/test/8-kyu/1-simpleMultiplication.test.ts
+++ Codewars/typescript/test/8-kyu/1-simpleMultiplication.test.ts
@@ -1,13 +1,11 @@
 import { simpleMultiplication } from "../../8-kyu/1-simpleMultiplication";
 
-describe("Simple multiplication", function () {
-  describe("Simple multiplication", () => {
-    it("Fixed test", () => {
-      expect(simpleMultiplication(2)).toBe(16);
-      expect(simpleMultiplication(1)).toBe(9);
-      expect(simpleMultiplication(8)).toBe(64);
-      expect(simpleMultiplication(4)).toBe(32);
-      expect(simpleMultiplication(5)).toBe(45);
-    });
+describe("Simple multiplication", () => {
+  it("Fixed test", () => {
+    expect(simpleMultiplication(2)).toBe(16);
+    expect(simpleMultiplication(1)).toBe(9);
+    expect(simpleMultiplication(8)).toBe(64);
+    expect(simpleMultiplication(4)).toBe(32);
+    expect(simpleMultiplication(5)).toBe(45);
   });
 });

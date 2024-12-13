9e7d87ed3f85858476fbec68a741486f7ab2bcfe
aimanyusufw <aimanyusufdev@gmail.com>
2024-12-13 08:08:54 +0700

    add : code other user method

--- Codewars/python/5-kyu/6_first_non_repeating_letter.py
+++ Codewars/python/5-kyu/6_first_non_repeating_letter.py
@@ -22,6 +22,13 @@ def first_non_repeating_letter(s):
 # Method : 2
     
 # Other user method
+def first_non_repeating_letter(string):
+    string_lower = string.lower()
+    for i, letter in enumerate(string_lower):
+        if string_lower.count(letter) == 1:
+            return string[i]
+            
+    return ""
     
 # Debug and log
 print(first_non_repeating_letter('a'))
